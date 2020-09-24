fade551747047f10086c6785c80b645df534e160
Tam Dang <tamdangnadmat@gmail.com>
2020-09-24 10:13:17 -0700

    black, version minimums for scipy and torch

--- distributions/ops/ive.py
+++ distributions/ops/ive.py
@@ -2,11 +2,13 @@ import scipy.special
 import torch
 import numpy as np
 
+
 class Ive(torch.autograd.Function):
     """
     The modified Bessel of the first kind, I_v(x), exponentially scaled by
     e^(-x) and made differentiable with respect to the continuous variable, x.
     """
+
     @staticmethod
     def forward(ctx, v: float, x: float):
         ctx._v = v
@@ -16,19 +18,22 @@ class Ive(torch.autograd.Function):
 
         # TODO: Original paper uses `np.close(v, 0)` and `np.close(v, 1)` to
         # default to scipy.special.i0e and scipy.special.i0e. Is this needed?
-        output = scipy.special.ive(v, x_cpu, dtype=x_cpu.dtype)
+        output = scipy.special.ive(
+            v, x_cpu, dtype=x_cpu.dtype
+        )  # pylint: disable=no-member
 
         return torch.Tensor(output).to(x.device)
 
     @staticmethod
     def backward(ctx, grad_output):
         v = ctx._v
-        x, = ctx.saved_tensors
+        (x,) = ctx.saved_tensors
 
         # Only compute a gradient for x, return None for order, v.
         return (
             None,
-            grad_output * (Ive.apply(v - 1, x) + Ive.apply(v, x) * ((v + x) / x))
+            grad_output * (Ive.apply(v - 1, x) + Ive.apply(v, x) * ((v + x) / x)),
         )
+
 
 ive = Ive.apply
